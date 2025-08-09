f88cc8034f524763856ea093b82b00b7e1cb646a
Krasimir Petranov <83006302+Krasimir0@users.noreply.github.com>
2025-08-09 16:11:48 +0100

    Finish exercise 1

--- 1.Spellbook Unraveling.js
+++ 1.Spellbook Unraveling.js
@@ -1,5 +1,38 @@
 function solve(arr) {
-   
+    let mistary = arr.shift();
+
+    let command = arr.shift().split("!");
+
+    let alreadyReversed = false;
+    while (command[0] !== "End") {
+            if (command[0] === "RemoveEven") {
+        let spell = '';
+            for (let index = 0; index < mistary.length; index+=2) {
+                let char = mistary[index];    
+                spell += char;
+            }
+            mistary = spell;
+            console.log(mistary);
+        } else if (command[0] === "TakePart") {
+            let fromIndex = Number(command[1]);
+            let toIndex = Number(command[2]);
+            mistary = mistary.substring(fromIndex, toIndex);
+            console.log(mistary);
+        } else if (command[0] === "Reverse") {
+            let substring = command[1];
+            if (substring && alreadyReversed === false) {
+            mistary = mistary.replace(substring, '');
+            substring = substring.split('').reverse().join('');
+            mistary += substring;
+            alreadyReversed = true;
+            console.log(mistary);
+            } else {
+                console.log("Error");
+            }
+        }
+        command = arr.shift().split("!");
+    }
+    console.log(`The concealed spell is: ${mistary}`);
 }
 
 
